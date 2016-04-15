4329299636b6f19a5ef515a76ec8a39864bec2da
probajany <probajany@gmail.com>
2016-04-15 13:23:24 +0000

    Button CSS updated

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var todos = new Array;
+show();
 
 function show() {
+    if (todos.length > 0) {
     var fill = '<ul>';
     for (var i = 0; i < todos.length; i += 1) {
         fill += '<li>' + todos[i] + ' <button class=\'rem\' id=\'' + i + '\'>x</button></li>';
     };
     fill += '</ul>';
-    
+    } else {
+        fill = '<h2>There is nothing to do!</h2>';
+    }
     document.getElementById("toDoList").innerHTML = fill;
     var buttons = document.getElementsByClassName('rem');
     for (var n = 0; n < buttons.length; n += 1) {
